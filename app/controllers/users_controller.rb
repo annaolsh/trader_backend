1433807676133f55e51c588a77d5f5b2403cc60a
github username <github email address>
2017-06-17 13:20:26 -0400

    changed columns profit, wallet amount to floats

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,8 +29,19 @@ protect_from_forgery :except => [:create]
 
   def show
     user = User.find(params[:id])
-    render json: user
+    actions = user.user_actions
+    wallet = user.wallets.first.amount
+    render json: {user: user, actions: actions, wallet: wallet}
   end
-
+  # def create
+  #   action = UserAction.new(action_params)
+  #   action.save
+  #   user = User.find(params[:user_action][:user_id])
+  #   action.user = user
+  #   wallet = action.user.wallets.first
+  #   wallet.amount = params[:user_action][:wallet]
+  #   wallet.save
+  #   render json: {action: action, wallet: wallet}
+  # end
 
 end
